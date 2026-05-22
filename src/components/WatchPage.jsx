f1baa74ebac84337abda2eae0efbe3bb2091db62
shubhamchauhan639 <shubhamchauhan12196@gmail.com>
2026-05-22 23:53:02 +0530

    fetch video data

--- src/components/WatchPage.jsx
+++ src/components/WatchPage.jsx
@@ -1,29 +1,79 @@
-import React, { use, useEffect } from 'react'
-import { useDispatch } from 'react-redux'
-import { closeMenu } from '../utils/appSlice'
-import { useSearchParams } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { closeMenu } from "../utils/appSlice";
+import { useSearchParams } from "react-router-dom";
+import { key, YOUTUBE_VIDEOS_API } from "../utils/constants";
 
 const WatchPage = () => {
-  const [searchParams]= useSearchParams();
-  console.log(searchParams.get("v"))
-  const dispatch = useDispatch()
+  const [searchParams] = useSearchParams();
+  const videoId = searchParams.get("v");
+
+  const dispatch = useDispatch();
+
+  const [videoData, setVideoData] = useState(null);
+
   useEffect(() => {
-    dispatch(closeMenu())
-  },[])
+    dispatch(closeMenu());
+    getVideoData();
+  }, []);
+
+ const getVideoData = async () => {
+  const data = await fetch(
+    "https://www.googleapis.com/youtube/v3/videos?part=snippet,statistics&id=" +
+      searchParams.get("v") +
+      "&key=" +
+     key
+  );
+
+  const json = await data.json();
+
+  console.log(json);
+
+
+  if (!json.items || json.items.length === 0) return;
+
+  console.log(json.items[0]);
+   setVideoData(json.items[0]);
+};
+  if (!videoData) return null;
+
+  const { snippet, statistics } = videoData;
 
   return (
-    <div className="px-2 py-4">
-  <iframe
-  width="100"
-  height="500"
-    className="rounded-2xl shadow-2xl w-[1000px] h-[550px]"
-    src={`https://www.youtube.com/embed/${searchParams.get("v")}`}
-    title="YouTube video player"
-    frameBorder="0"
-    allowFullScreen
-  ></iframe>
-</div>
-  )
-}
+    <div className="p-5">
+      {/* Video */}
+      <iframe
+        className="rounded-2xl w-full max-w-5xl aspect-video"
+        src={`https://www.youtube.com/embed/${videoId}`}
+        title="YouTube video player"
+        allowFullScreen
+      ></iframe>
 
-export default WatchPage
+      {/* Video Details */}
+      <div className="max-w-5xl mt-4">
+        <h1 className="text-2xl font-bold">
+          {snippet.title}
+        </h1>
+
+        <div className="flex items-center justify-between mt-4">
+          <div>
+            <h2 className="font-semibold text-lg">
+              {snippet.channelTitle}
+            </h2>
+          </div>
+
+          <div className="flex gap-4">
+            <p> Like :{statistics.likeCount}</p>
+            <p> Views : {statistics.viewCount}</p>
+          </div>
+        </div>
+
+        <div className="bg-gray-100 p-4 rounded-xl mt-4">
+          <p>{snippet.description}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default WatchPage;
