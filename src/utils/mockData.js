71dc85d5205e84b20b15ce4e5106daabb47e63a5
shubhamchauhan639 <shubhamchauhan12196@gmail.com>
2026-05-31 13:07:54 +0530

    update mockdata

--- src/utils/mockData.js
+++ src/utils/mockData.js
@@ -19,6 +19,34 @@ const commentsData = [
     text: "React is awesome",
     replies: [],
   },
+
+  {
+    id: 4,
+    name: "karan",
+    text: "nice",
+    replies: [],
+  },
+
+  {
+    id: 5,
+    name: "deep",
+    text: "hello kese ho",
+    replies: [],
+  },
+
+  {
+    id: 6,
+    name: "Spider Man",
+    text: "great power comes with great responsibility",
+    replies: [],
+  },
+
+  {
+    id: 7,
+    name: "Iron Man",
+    text: "I am Iron Man",
+    replies: [],
+  },
 ];
 
 export default commentsData
