f66ecf622564e6038730d8b273055eff3d9491d2
shubhamchauhan639 <shubhamchauhan12196@gmail.com>
2026-06-03 15:20:31 +0530

    .

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,7 @@
 export const logo = "/ChatGPT.png"
-export const key = 
+export const key = import.meta.env.VITE_Youtube_KEY;
 export const YOUTUBE_VIDEOS_API ="https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
 key;
 export const Youtube_Search_Api = "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q="
 export const search_logo = "/icons8.svg "
+export const LIVE_CHAT_COUNT = 25;
