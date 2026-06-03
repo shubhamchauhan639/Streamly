58803b9af6f77788be083e9257bd027f66aca658
shubhamchauhan639 <shubhamchauhan12196@gmail.com>
2026-06-03 20:23:15 +0530

    .

--- src/components/LiveChat.jsx
+++ src/components/LiveChat.jsx
@@ -18,7 +18,7 @@ const LiveChat = () => {
       dispatch(
         addMessage({
           name: generateRandomName(),
-          message: makeRandomMessage(20) + " 🚀",
+          message: makeRandomMessage(20) ,
         })
       );
     }, 2000);
